7796e053bc7d708ae0978627be6325cded34791b
Gaweł Kazimierczuk <git@kazigk.me>
2024-07-07 18:15:51 +0200

    fix: NoClassDefFoundError in RTP_HuskTowns

--- src/main/java/me/SuperRonanCraft/BetterRTP/references/depends/regionPlugins/RTP_HuskTowns.java
+++ src/main/java/me/SuperRonanCraft/BetterRTP/references/depends/regionPlugins/RTP_HuskTowns.java
@@ -4,19 +4,12 @@ import net.william278.husktowns.api.BukkitHuskTownsAPI;
 import org.bukkit.Location;
 
 public class RTP_HuskTowns implements RegionPluginCheck {
-
-    private final BukkitHuskTownsAPI huskTownsAPI;
-
-    public RTP_HuskTowns() {
-        this.huskTownsAPI = BukkitHuskTownsAPI.getInstance();
-    }
-
     @Override
     public boolean check(Location loc) {
         boolean result = true;
         if (REGIONPLUGINS.HUSKTOWNS.isEnabled()) {
             try {
-                result = !huskTownsAPI.getClaimAt(loc).isPresent();
+                result = !BukkitHuskTownsAPI.getInstance().getClaimAt(loc).isPresent();
             } catch (Exception e) {
                 e.printStackTrace();
             }
